fba9281a1e16541431c1ef05dda814515a4949b9
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-01-12 21:08:57 +0200

    dsa

--- app/head.js
+++ app/head.js
@@ -3,6 +3,14 @@ export default function Head() {
     <>
       <title>mozsm</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+      />
+      <meta name="robots" content="noindex,nofollow" />
+      <meta name="robots" content="all" />
+      <meta name="googlebot" content="noindex,nofollow" />
+
       <link rel="icon" href="/favicon.ico" />
     </>
   )
