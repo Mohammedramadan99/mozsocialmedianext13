52ac681fced83f99518a0ee9a4b68a9ba90eee20
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-01-22 21:18:02 +0200

    dsa

--- components/UserDetails/Friend.jsx
+++ components/UserDetails/Friend.jsx
@@ -6,7 +6,7 @@ export default function Friend({ user })
     return (
         <Link href={`/user/${user._id}`} className='user__bottom__sidebar__friendsCard__friendsList__friend'>
             <div className="user__bottom__sidebar__friendsCard__friendsList__friend__img">
-                <img src={user?.profilePhoto} alt="img" />
+                <img src={user?.image} alt="img" />
             </div>
             <div className="user__bottom__sidebar__friendsCard__friendsList__friend__name">{user.name}</div>
         </Link>
