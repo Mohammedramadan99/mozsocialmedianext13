4d42e2183dd63ea0c362bcfd2fe1837a50cec0c0
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-01-22 22:44:00 +0200

    Ds

--- app/user/[id]/page.jsx
+++ app/user/[id]/page.jsx
@@ -5,16 +5,25 @@ import { motion } from "framer-motion";
 import { useSession } from 'next-auth/react';
 import { useDispatch, useSelector } from 'react-redux';
 import { LoggedInUserAction } from '../../../store/usersSlice';
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 function userDetails(props)
 {
     const {data:session} = useSession()
     const dispatch = useDispatch()
+    const router = useRouter()
     const {id} = props.params
     const {
         userAuth,
     } = useSelector(state => state.users);
     !userAuth?._id && session?.user?.email && dispatch(LoggedInUserAction({email:session?.user?.email}))
+    useEffect(() => {
+      if (userAuth === null || !userAuth || userAuth === {})
+      {
+          router.push('/login')
+      }
+    }, [userAuth,session])
   return (
     <motion.div variants={routerAnimation}
         initial="initial"
